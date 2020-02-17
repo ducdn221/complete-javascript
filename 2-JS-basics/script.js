4fda6e5db290c3668120a73514c1d8044e1e1174
dnduc <dnduc@cmc.com.vn>
2020-02-17 21:40:50 +0700

    coding 3

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -130,3 +130,23 @@ function example() {
 var whatDoYouDo = function(job, firstName) {
 
 }
+
+
+/******************************
+* CODING CHALLENGE 3
+*/
+function tipCaculator(bill) {
+    var percentage;
+    if(bill < 50) {
+        percentage = .2;
+    }else if(bill >=50 && bill <= 200) {
+        percentage = .15;
+    }else{
+        percentage = .1;
+    }
+    return percentage*bill;
+}
+
+var bills = [124,48,268];
+var tips = [tipCaculator(bills[0]),tipCaculator(bills[1]),tipCaculator(bills[2])];
+var finalValues = [bills[0] + tips[0],bills[1] + tips[1],,bills[2] + tips[2]]
