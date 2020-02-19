28cdb942c3fa830e504e09d1a70c6a1a49980078
dnduc <dnduc@cmc.com.vn>
2020-02-19 17:31:05 +0700

    hoisting

--- 3-How-JS-works/script.js
+++ 3-How-JS-works/script.js
@@ -1,9 +1,29 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+console.log(calculateAge(1990));
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
+// console.log(retirement(1958));
+var retirement = function(year) {
+    console.log(65-(2016-year));
+}
 
+//variables
 
+console.log(age);
+var age= 23;
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);
+
+console.log(window);
 
 
 
