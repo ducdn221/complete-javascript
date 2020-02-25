4373bc13177d23b996dc392d02532039d0da7d39
dnduc <dnduc@cmc.com.vn>
2020-02-25 20:49:18 +0700

    closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -58,19 +58,89 @@
 
     //----------------Function
 
-    var age = 27;
-    var obj = {
-        name: 'Jonas',
-        city: 'Lisbon'
-    };
+    // var age = 27;
+    // var obj = {
+    //     name: 'Jonas',
+    //     city: 'Lisbon'
+    // };
 
-    function change(a,b) {
-        a = 30;
-        b.city = 'San fransisco'
+    // function change(a,b) {
+    //     a = 30;
+    //     b.city = 'San fransisco'
+    // }
+
+    // change(age,obj);
+    // console.log(age);
+    // console.log(obj)
+
+
+
+// ----------------------------- Lecture: Passing function as arguments
+// var years = [1994,2002,1999,1995,1993,1992,1969];
+
+// function arrayCalc(arr,fn) {
+//     var arrRes = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calcAge(el) {
+//     return 2020 - el;
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+//     if(el >=18 && el <= 81) {
+//         return Math.round(206.9 -(0.67*el));
+//     }else {
+//         return -1;
+//     }
+// }
+
+// console.log(arrayCalc(years,calcAge));
+// var age = arrayCalc(years,calcAge)
+// console.log(arrayCalc(age,isFullAge));
+// console.log(arrayCalc(age,maxHeartRate));
+
+// ----------------------------- Lecture: IIFE(Immediately Invoked Function Expression)
+
+
+(function() {
+    var score = Math.random() * 10;
+    console.log(score > 5);
+})();
+
+// ----------------------------- Lecture: Closure
+
+function retirement(retirementAge) {
+    var a  =' years left until retirement';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
     }
+}
 
-    change(age,obj);
-    console.log(age);
-    console.log(obj)
+var retirementUS = retirement(66);
+retirementUS(1990);
 
+function interviewQuestion(job) {
+    return function(name) {
+        if(job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        }else if(job === 'teacher') {
+            console.log('What subject do you teach, ' + name+ '?');
+        }else {
+            console.log('Hello ' + name +', What do you do?' );
+        }
+    }
+}
 
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('Duc');
+var designerQuestion = interviewQuestion('designer');
+designerQuestion('John');
