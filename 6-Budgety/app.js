41ca57ae6c147b5f57e8762291819e2d89aa0cef
dnduc <dnduc@cmc.com.vn>
2020-02-28 17:49:56 +0700

    budgety create get input

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,22 +1,50 @@
-var budgetController = (function() {
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  };
-  
-  return {
-      publicTest: function(b) {
-          console.log(add(b));
-      }
-  }
-})();
-
+var budgetController = (function() {})();
 
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
 
+  return {
+    getinput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+       if (event.keyCode === 13 || event.which === 13) {
+         ctrlAddItem();
+       }
+     });
+  };
+ 
+  var ctrlAddItem = function () {
+    // 1. Get the filed input data
+    var input = UICtrl.getinput();
+    console.log(input);
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  };
 
 })(budgetController, UIController);
