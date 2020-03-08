18437ed79578774ce0be25362669962bf3635abd
dnduc <dnduc@cmc.com.vn>
2020-03-08 11:24:15 +0700

    paging

--- 9-Forkify/src/js/index.js
+++ 9-Forkify/src/js/index.js
@@ -1,4 +1,42 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from './views/base';
+import * as searchView from './views/searchView';
+/**
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-console.log(`I import ${num} in another module12344`)
+const controlSearch = async () => {
+    // 1) Get query form view
+    const query = searchView.getInput();
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const gotoPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result,gotoPage);
+    }
+})
---
+++ 9-Forkify/src/js/views/searchView.js
@@ -0,0 +1,92 @@
+import { elements } from './base'
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResList.textContent = '';
+    elements.searchResPages.textContent = '';
+}
+
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    if (title.length > limit) {
+        const newTitle = [];
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0)
+
+        return `${newTitle.join(' ')} ...`
+    }
+    return title;
+
+}
+
+const renderRecipe = recipe => {
+    const markup = ` 
+    <li>
+        <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="${recipe.title}">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+    `;
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+
+
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage)
+    let button;
+    if (page === 1 && pages > 1) {
+        //Only show button next
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        //Show next and previous button
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `
+
+    } else if (page === pages && pages > 1) {
+        // only show button previous
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+}
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * 10;
+    const end = page * resPerPage;
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render button
+    renderButtons(page, recipes.length, resPerPage);
+};
